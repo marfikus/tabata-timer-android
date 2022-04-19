b800de2d6b60b6202de85565f90b2693a776395c
Alexey Zaytsev <alex.rv9rh@gmail.com>
2022-04-19 12:35:11 +0500

    Add App class

---
+++ app/src/main/java/com/github/marfikus/tabatatimer/App.java
@@ -0,0 +1,13 @@
+package com.github.marfikus.tabatatimer;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
